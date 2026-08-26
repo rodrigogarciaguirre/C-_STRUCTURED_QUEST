57dae3ebf1cd688f29c5c3e908dd35ec36562375
RodrigoGarciaguirre <rodrigarciaguirre@gmail.com>
2026-08-26 11:27:08 -0600

    Reinicio del proyecto

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double numero = 199999;
+        Console.WriteLine($"Numero que coma mierda : {numero}");
+    }
+}
