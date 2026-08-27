9a6d1b0be79d12e1f107cfc12e36e970b5432db1
RodrigoGarciaguirre <rodrigarciaguirre@gmail.com>
2026-08-27 11:38:53 -0600

    Nivel 10 Completado

---
+++ Nivel-10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Nivel10;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Procesa una secuencia de códigos: ignora "ERROR" con continue y detén el procesamiento al encontrar "EXIT" con break.
+        string[] codigos = ["Codigo1","Codigo2","EXIT","Codigo3","ERROR"];
+
+        foreach(string codigo in codigos)
+        {
+
+            if(codigo == "ERROR")
+            {
+                Console.WriteLine("[SE IGNORA]: El codigo tiene un error.");
+                continue;
+            }
+            if(codigo == "EXIT")
+            {
+                Console.WriteLine("[EXIT]: El codigo se detiene");
+                break;
+            }
+            Console.WriteLine("El codigo funciona correctamente.");
+        }
+    }
+}
