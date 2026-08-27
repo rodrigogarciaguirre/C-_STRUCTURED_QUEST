b9e4e9db75feb1428186d316e6efe83e2bb5d7a2
RodrigoGarciaguirre <rodrigarciaguirre@gmail.com>
2026-08-27 11:29:52 -0600

    Nivel-9 Completado

---
+++ Nivel-9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Nivel9;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Recorre una colección de nombres con foreach y muestra únicamente los que tengan más
+        //de cuatro caracteres, combinando foreach + if + String.
+
+        string[] nombres = ["Rodrigo", "Javier","Darwin","Angel", "Ana"];
+
+        foreach(string nombre in nombres)
+        {
+            if(nombre.Length > 4)
+            {
+                Console.WriteLine($"Nombres con mas de 4 letras: {nombre}");
+            }
+        }
+        Console.WriteLine("Presiona cualquier tecla...");
+        Console.ReadKey();
+
+    }
+}
