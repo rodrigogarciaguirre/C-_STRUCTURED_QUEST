c83e569b053570c1f87b3f71c2bf37661b550f00
RodrigoGarciaguirre <rodrigarciaguirre@gmail.com>
2026-08-27 11:52:33 -0600

    Nivel-11 Completado

---
+++ Nivel-11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Nivel11;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		//Solicita un nombre completo y muestra el nombre limpio, la cantidad de caracteres, y
+        //sus versiones en mayúsculas y minúsculas. Debe manejar espacios innecesarios.
+        Console.WriteLine("Escriba su nombre completo:  ");
+        string nombre = Console.ReadLine();
+
+        string nombrelimpio = nombre.Trim();
+        Console.WriteLine("-----------------------------");
+        Console.WriteLine("CARACTERISTICAS DEL NOMBRE");
+        Console.WriteLine("-----------------------------");
+        Console.WriteLine($"Nombre limpio           : {nombrelimpio}");
+        Console.WriteLine($"Cantidad de caracteres  : {nombrelimpio.Length}");
+        Console.WriteLine($"Version en mayusculas   : {nombrelimpio.ToUpper()}");
+        Console.WriteLine($"Su version en minusculas: {nombrelimpio.ToLower()}");
+
+        Console.WriteLine("Presione cualquier tecla...");
+        Console.ReadKey();
+	}
+}
+
